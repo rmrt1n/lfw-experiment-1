77f0f53664e395505e86fcd8060013f7ce3d2b38
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-04-19 19:34:24 +0700

    feat: switch to solidstart basic template & add tailwindcss

---
+++ apps/roff/src/routes/index.jsx
@@ -0,0 +1,12 @@
+import { Title } from '@solidjs/meta'
+import Counter from '~/components/counter'
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Hello World</Title>
+      <h1>Hello world!</h1>
+      <Counter />
+    </main>
+  )
+}
