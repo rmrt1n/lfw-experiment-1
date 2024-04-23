9438a1add381e98e13b79b845751d8e585c4b4f7
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-04-23 19:04:57 +0700

    fix: tx get's incremented for every attr. it should only only inc every update

--- apps/roff/src/lib/frain/db.js
+++ apps/roff/src/lib/frain/db.js
@@ -9,16 +9,18 @@ export function createTransactor(db, setDb, ns) {
 }
 
 function batchUpdate(db, ns, id, kvs) {
-  return Object.keys(kvs).reduce((acc, k) => update(acc, id, `${ns}/${k}`, kvs[k], true), db)
+  const nextTx = db.maxTx + 1
+  return {
+    ...Object.keys(kvs).reduce((acc, k) => update(acc, id, `${ns}/${k}`, kvs[k], nextTx, true), db)
+  }
 }
 
-function update(db, e, a, v, op) {
-  const nextTx = db.maxTx + 1
+function update(db, e, a, v, tx, op) {
   return {
     ...db,
     eavt: { ...db.eavt, [e]: { ...db.eavt[e], [a]: v } },
     aevt: { ...db.aevt, [a]: { ...db.aevt[a], [e]: v } },
-    maxTx: nextTx,
-    storage: { ...db.storage, [e]: { ...db.storage[e], [a]: { value: v, tx: nextTx, op } } }
+    maxTx: tx,
+    storage: { ...db.storage, [e]: { ...db.storage[e], [a]: { value: v, tx, op } } }
   }
 }
