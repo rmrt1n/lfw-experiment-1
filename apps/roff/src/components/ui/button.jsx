7c1af94c7eebe6143017d1f98d3aa35ca0b3b91c
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-04-27 16:27:41 +0700

    feat: update button styles

--- apps/roff/src/components/ui/button.jsx
+++ apps/roff/src/components/ui/button.jsx
@@ -6,7 +6,8 @@ export const button = cva({
     'inline-flex items-center justify-center gap-2 flex-shrink-0',
     'h-8 px-2.5 py-0.5 border rounded-md ring-offset-1',
     'font-medium text-center no-underline align-center whitespace-nowrap leading-tight',
-    '[&:not(:active)]:shadow-sm active:shadow-[inset_0_1px_2px_0_rgba(0,0,0,0.06);]',
+    '[&:not(:active)]:shadow-[0_1px_2px_0_rgba(0,0,0,0.05),inset_0_1px_0_0_rgba(255,255,255,0.5)]',
+    'active:shadow-[inset_0_2px_2px_0_rgba(0,0,0,0.05)]',
     'cursor-default'
   ],
   variants: {
@@ -14,12 +15,12 @@ export const button = cva({
       default: [
         'bg-gradient-to-b from-neutral-50 to-neutral-100 border-neutral-300 text-neutral-800',
         'hover:border-neutral-400',
-        'active:from-neutral-100 active:to-neutral-100 active:border-neutral-300'
+        'active:from-neutral-100 active:to-neutral-100 active:border-neutral-300',
       ],
       primary: [
         'bg-gradient-to-b from-indigo-500 to-indigo-600 border-indigo-600 text-neutral-100',
-        'hover:from-indigo-500/95 hover:to-indigo-600/95 hover:border-indigo-800',
-        'active:from-indigo-600 active:to-indigo-600/95 active:border-indigo-600'
+        'hover:from-indigo-500/95 hover:to-indigo-600/95 hover:border-indigo-700',
+        'active:from-indigo-600 active:to-indigo-600/95 active:border-indigo-600',
       ],
     },
   },
